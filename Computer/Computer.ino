85aba35b78dda84997fb3ff33eaa1408b01c117c
alexisszam <zamoraa.alexiss@ciencias.unam.mx>
2024-06-21 13:46:50 +0200

    Create Computer.ino

---
+++ Computer/Computer.ino
@@ -0,0 +1,41 @@
+#include <WiFi.h>
+
+const char *ssid = "ESP32-AP";     // Name of the Wireless Arduino WiFi
+const char *password = "password"; // Password for the Wireless Arduino WiFi
+
+WiFiServer server(80);  // Port for the server
+
+void setup() {
+  Serial.begin(115200); // Initialize serial communication at 115200 bps
+  
+  // Configure the ESP32 as a WiFi access point
+  WiFi.softAP(ssid, password);
+
+  // Get the IP address of the WiFi access point
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("IP adress of the WiFi: ");
+  Serial.println(IP);
+
+  server.begin(); // Start the server
+}
+
+void loop() {
+  WiFiClient client = server.available(); // Check if a client has connected
+
+  if (client) {
+    Serial.println("Arduino connected");
+
+    while (client.connected()) {
+      if (client.available()) {
+        // Read data from the Wireless Arduino
+        String data = client.readStringUntil('\n');
+        
+        // Display the received data in the serial monitor
+        Serial.println(data);
+      }
+    }
+
+    client.stop(); // Stop the client
+    Serial.println("Arduino disconnected");
+  }
+}
